cdfe1687f1d57decef303ad892b753f60befb434
John Mettraux <jmettraux@gmail.com>
2013-06-05 20:59:22 +0900

    simplify spec/spec_base.rb

--- spec/spec_base.rb
+++ spec/spec_base.rb
@@ -5,27 +5,6 @@
 # Wed Mar 11 16:09:31 JST 2009
 #
 
-
-#
-# bacon
-
-$:.unshift(File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib')))
-
-#require 'rubygems'
 require 'fileutils'
-
-
-#$:.unshift(File.expand_path('~/tmp/bacon/lib')) # my own bacon for a while
-#
-#require 'bacon'
-#
-#puts
-#
-#Bacon.summary_on_exit
-
-
-#
-# rufus/lua
-
 require 'rufus/lua'
 
