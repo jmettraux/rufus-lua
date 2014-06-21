36aa42fc03dc832db8796b7d8932454fe9adbf44
Nathanael Jones <nathanael.jones@gmail.com>
2014-06-21 13:11:36 +0200

    Changed lua_pushlstring signature to use :pointer instead of :string Added 3 different ways to push a string to the stack. At one point, only stack_push_string_unit8 worked. Now all 3 work, perhaps due updating FFI? Added additional specs to test round-tripping from both sides, as well as verify that bytes after the first \0 aren't also \0 (since this happened during testing).

--- spec/string_spec.rb
+++ spec/string_spec.rb
@@ -30,8 +30,6 @@ describe 'lua strings' do
 
     it 'are not truncated when passed from Ruby to Lua and back' do
 
-      pending "Ruby FFI doesn't allow it"
-
       s = [ 65, 66, 0, 67, 68 ].pack('c*')
 
       f = @s.eval(%{
@@ -43,6 +41,59 @@ describe 'lua strings' do
 
       expect(f.call(s).to_h).to eq({ 's' => s, 'l' => 5 })
     end
+
+    it 'fetch nully strings from stack' do 
+      expect(@s.eval("return string.char(32,0,0,32)")).to eq("\x20\x00\x00\x20")
+      expect(@s.eval("return string.char(32,0,0,32)").length).to eq(4)
+    end
+
+    it 'verify ruby string behavior' do
+      nullstr = "\x20\x00\x00\x20"
+      expect(nullstr.size).to eq(4)
+      expect(nullstr.getbyte(3)).to eq(32)
+    end 
+
+    it 'push nully strings to stack' do 
+      nullstr = "\x20\x00\x00\x20"
+      @s.function "get_null_string" do
+        nullstr
+      end
+      expect(@s.eval("return string.len(get_null_string())")).to eq(4)
+      expect(@s.eval("return string.byte(get_null_string(),4)")).to eq(32)
+
+      expect(@s.eval("return (get_null_string() == string.char(32,0,0,32))")).to eq(true)
+      
+      str = @s.eval("return string.char(32,0,0,32)")
+      expect(str.length).to eq(4) 
+      expect(str).to eq(nullstr) 
+    end
+  
+    it 'roundtrips null bytes' do
+      #Here we verify roundtripping from both sides
+      #Note that length checking is just to make sure the nullcounters don't activate
+      output = {}
+      @s.function "save_out" do |k, v|
+        output[k] = v
+      end
+      @s.function "pull_in" do |k|
+        output[k]
+      end
+      @s.eval(%{
+        s = string.char(32,0,32)
+        save_out("copy",s)
+        s = pull_in("copy")
+        save_out("length",string.len(s))
+        save_out("byte0", string.byte(s,1))
+        save_out("byte1", string.byte(s,2))
+        save_out("byte2", string.byte(s,3))
+        save_out("copy2",s)})
+      expect(output["copy2"]).to eq("\x20\x00\x20")
+      expect(output["byte0"]).to eq(32.0)
+      expect(output["byte1"]).to eq(0.0)
+      expect(output["byte2"]).to eq(32.0)
+      expect(output["length"]).to eq(3)
+    end
+
   end
 end
 
